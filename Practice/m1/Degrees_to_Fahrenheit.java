cff03a61e36d546587e2253dd79e1c327fe2c31f
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-27 10:15:35 -0400

    Degrees_to_Fahrenheit.java

---
+++ Practice/m1/Degrees_to_Fahrenheit.java
@@ -0,0 +1,22 @@
+/** HelloWorld.java.
+ * @author Sai Kiran.
+ */
+public final class Degrees_to_Fahrenheit {
+    /**
+     * empty constructor.
+     */
+    private Degrees_to_Fahrenheit() {
+        // I'm not using this constructor
+
+    }
+        /**
+         * @main function to print Hello World.
+         * @param args is a parameter for this function
+         */
+    public static void main(final String[] args) {
+        int x = 50;
+        double f = ((x * 1.8) + 32);
+        System.out.println(f);
+    }
+
+}
