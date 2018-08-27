123c3d19662ac9d6a5b739aaa8594cb9651dc349
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-27 10:02:30 -0400

    Quotient_Remainder.java

---
+++ Practice/m1/Quotient_Remainder.java
@@ -0,0 +1,24 @@
+/** HelloWorld.java.
+ * @author Sai Kiran.
+ */
+public final class Quotient_Remainder {
+    /**
+     * empty constructor.
+     */
+    private Quotient_Remainder() {
+        // I'm not using this constructor
+
+    }
+        /**
+         * @main function to print Hello World.
+         * @param args is a parameter for this function
+         */
+    public static void main(final String[] args) {
+        int a = 6;
+        int b = 3;
+        int c = a / b;
+        int d = a % b;
+        System.out.println(c + " " + d);
+    }
+
+}
