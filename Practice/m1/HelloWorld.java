ac247669a0b4a8c5388371e30cde4761d83c09ce
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-27 09:11:37 -0400

    HelloWorld.java

---
+++ Practice/m1/HelloWorld.java
@@ -0,0 +1,20 @@
+/** HelloWorld.java.
+ * @author Sai Kiran.
+ */
+public final class HelloWorld {
+    /**
+     * empty constructor.
+     */
+    private HelloWorld() {
+        // I'm not using this constructor
+
+    }
+            /**
+        * @main function to print Hello World.
+         * @param args is a parameter for this function
+         */
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
