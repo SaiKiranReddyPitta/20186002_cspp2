9572fa442c98bbe75c1b8c143740d0389fddc2f4
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-27 10:34:41 -0400

    Solution.java

---
+++ assignment/Assignment-1/Solution.java
@@ -0,0 +1,19 @@
+/**Solution.java
+ * @author Sai Kiran
+ */
+public class Solution {
+	/**
+	 * empty constructor
+	 */
+	private Solution() {
+		// i'm not using this constructor
+	}
+	/**
+	 * @main function to print Name and Roll Number.
+     * @param args is a parameter for this function
+     */
+	public static void main(String[] args) {
+		System.out.println("Name : Sai Kiran P, Roll Number : 20186002");
+
+	}
+}
