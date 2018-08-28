47329916f0c9242138c008a0158be78a1f0b1305
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-28 12:40:00 -0400

    bigger_equal_smaller.java

--- assignment/m2/practice/bigger_equal_smaller.java
+++ assignment/m2/practice/bigger_equal_smaller.java
@@ -1,20 +1,31 @@
-import java.util.*;
-public class bigger_equal_small{
-	public static void check(int a,int b) {
-		if(a>b){
-			System.out.println("Bigger");
-		}
-		else if(a<b){
-			System.out.println("Smaller");
-		}
-		else{
-			System.out.println("Equal");
-		}
-	}
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int a = s.nextInt();
-		int b= s.nextInt();
-		check(a,b);
-	}
+import java.util.Scanner;
+/**
+ *@authorArepallyrakesh
+*/
+public final class bigger_eual_smaller {
+    /*
+    Do not modify this main function.
+    */
+      /**
+     * Empty Constructor.
+    */
+    private bigger_eual_smaller() {
+        //not using this constructor
+    }
+    /**
+     * Main function to print.
+     * @param args is the parameter for this function
+    */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+        if (num1 > num2) {
+            System.out.println(num1 + " is Bigger than " + num2);
+        } else if (num1 < num2) {
+            System.out.println(num1 + " is Smaller than " + num2);
+        } else {
+            System.out.println(num1 + "," + num2 + " are equal");
+        }
+    }
 }
