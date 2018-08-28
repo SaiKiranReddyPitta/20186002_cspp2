7fd4a09903c7902c86eed63d19fa33adf8aede2f
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-28 08:59:37 -0400

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignment/m2/Assignment-3/Solution.java
+++ assignment/m2/Assignment-3/Solution.java
@@ -15,16 +15,17 @@ public final class Solution {
     /**
      *@param args variable
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        System.out.println((long)power(base, exponent));
+        System.out.println((long) power(base, exponent));
     }
     /**
      *Need to write the power func and prints output.
      *@param base variable base
      *@param exponent variable exponent
+     *@return double
      */
     static double power(final int base, final int exponent) {
         return Math.pow(base, exponent);
