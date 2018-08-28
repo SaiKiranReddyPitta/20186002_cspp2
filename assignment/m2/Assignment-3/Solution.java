5f3448de17105ee6dbe6b3f5e1f233146381b2bf
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-28 08:48:46 -0400

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignment/m2/Assignment-3/Solution.java
+++ assignment/m2/Assignment-3/Solution.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
-
-public class Solution {
+/**Solution.java.
+ *do not modify this main function.
+ * @author Sai Kiran Reddy Pitta.
+ */
+public final class Solution {
     /*
     Do not modify this main function.
     */
+    private Solution() {
+        // we are not using this constructor.
+    }
+    /**
+     *@param args variable
+     */
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
         System.out.println((long)power(base,exponent));
     }
-    /*
-    Need to write the power function and print the output.
-    */
+    /**
+     *Need to write the power func and prints output.
+     *@param base variable base
+     *@param exponent variable exponent
+     */
     static double power(int base, int exponent) {
         return Math.pow(base, exponent);
     }
