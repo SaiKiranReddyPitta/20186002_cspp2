4da9887c936bdd4cec2a07326c1c99b3f6dfbf3c
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-28 08:41:23 -0400

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignment/m2/Assignment-2/Solution.java
+++ assignment/m2/Assignment-2/Solution.java
@@ -13,7 +13,7 @@ public final class Solution {
     /**
      *@param args variable
      */
-    public final static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
