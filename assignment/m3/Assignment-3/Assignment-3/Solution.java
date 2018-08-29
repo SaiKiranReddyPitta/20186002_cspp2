d702a647f5f0f5505ca0047e87351421c042df34
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-29 05:03:32 -0400

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignment/m3/Assignment-3/Assignment-3/Solution.java
+++ assignment/m3/Assignment-3/Assignment-3/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 /**
  * { function_description }
  *
- * @param      args  The arguments
+ * @param      args  The arguments.
  */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -24,9 +24,14 @@ public class Solution {
 	Need to write the gcd function and print the output.
 	*/
     static int gcd(final int n1, final int n2) {
+        /**
+         * { var_description }
+         * @param      n1    The n1.
+         * @param      n2    The n2.
+         */
     int gcd = 1;
     for (int i = 1; i <= n1 && i <= n2; i++) {
-            // Checks if i is factor of both integers
+            // Checks if i is factor of both integers.
             if (n1 % i == 0 && n2 % i == 0) {
                 gcd = i;
             }
