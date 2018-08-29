0ce6f599d161424da426e87e80f42a65efa37171
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-29 05:14:42 -0400

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignment/m3/Assignment-3/Assignment-3/Solution.java
+++ assignment/m3/Assignment-3/Assignment-3/Solution.java
@@ -30,11 +30,14 @@ public final class Solution {
     /**
 	 *Need to write the gcd function and print the output.
 	 */
-        /**.
-         * { var_description }
-         * @param      n1    The n1.
-         * @param      n2    The n2.
-         */
+    /**
+     * { function_description }
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
     static int gcd(final int n1, final int n2) {
     int gcd = 1;
     for (int i = 1; i <= n1 && i <= n2; i++) {
