6b70dc6d3c54f91c3ab9433e37d27b7683026a25
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-29 05:00:00 -0400

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignment/m3/Assignment-3/Assignment-3/Solution.java
+++ assignment/m3/Assignment-3/Assignment-3/Solution.java
@@ -6,23 +6,25 @@
  */
 import java.util.Scanner;
 /**
- *Do not modify this main function.
+ * Do not modify this main function.
  */
 public class Solution {
-
+/**
+ * { function_description }
+ *
+ * @param      args  The arguments
+ */
     public static void main(String[] args) {
-
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1,n2));
     }
-    
     /*
 	Need to write the gcd function and print the output.
 	*/
     static int gcd(final int n1, final int n2) {
-        int gcd = 1;
+    int gcd = 1;
     for (int i = 1; i <= n1 && i <= n2; i++)
         {
             // Checks if i is factor of both integers
