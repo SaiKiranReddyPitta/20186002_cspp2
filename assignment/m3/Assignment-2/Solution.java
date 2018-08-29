d87b1f8093a8d2a2ca2bab5157bcca289bcf8d39
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-29 04:29:20 -0400

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignment/m3/Assignment-2/Solution.java
+++ assignment/m3/Assignment-2/Solution.java
@@ -1,25 +1,44 @@
-
+/**Solution.java.
+ * @author Sai Kiran.
+*/
+/**.
+ * Class for solution.
+ */
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
-public class Solution {
-/* Fill the main function to print the number of 7's between 1 to n*/
+/**.
+ Do not modify this main function.
+ */
+public final class Solution {
+/**.
+ * Constructs the object.
+ */
+private static final int a = 7;
+private static final int b = 10;
+/**.
+ * Empty Constructor.
+ */
+    private Solution(){
+          //Constructor
+    }
+/**.
+ * Fill the main function to print the number of 7's between 1 to n.
+ * @param      args  The arguments.
+ */
     public static void main(String[] args) {
-
         Scanner s=new Scanner(System.in);      
         int n = s.nextInt();
         int count = 0;
         int i = 0;
         int j;
+
         for (i = 0; i <= n; i++) {
-        	j = i;
-        	while (j != 0) {
-        		if (j % 10 == 7)
-        			count ++;
-        		j /= 10;
-        	}
+            j = i;
+            while (j != 0) {
+                if (j % b == a)
+                    count ++;
+                j /= b;
+            }
         }
-    	System.out.println(count); 
+        System.out.println(count); 
     }
 }
