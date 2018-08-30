2d3578b415170a1b28d0588a3035dda56001e9d6
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-30 14:44:59 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- assignment/m4/Assignment-2/Assignment-2/Solution.java
+++ assignment/m4/Assignment-2/Assignment-2/Solution.java
@@ -1,40 +1,45 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
-       Scanner s = new Scanner(System.in);
+Scanner scanner = new Scanner(System.in);
        //System.out.print("Enter number of rows: ");
-       int rows = s.nextInt();
+       int rows = scanner.nextInt();
        //System.out.print("Enter number of columns: ");
-       int columns = s.nextInt();
-       int[][] a = new int[rows][columns];
-       int[][] b = new int[rows][columns];
-       //System.out.println("Enter the first matrix");
+       int columns = scanner.nextInt();
+       //System.out.println();
+       //System.out.println("Enter first matrix");
+       int[][] a = readMatrix(rows, columns);
+       //System.out.println();
+       //System.out.println("Enter second matrix");
+       int[][] b = readMatrix(rows, columns);
+       int[][] sum = add(a, b);
+       //System.out.println();
+       //System.out.println("A + B =");
+       System.out.println(sum);
+   }
+   public static int[][] readMatrix(int rows, int columns) {
+       int[][] result = new int[rows][columns];
+       Scanner s = new Scanner(System.in);
        for (int i = 0; i < rows; i++) {
            for (int j = 0; j < columns; j++) {
-               a[i][j] = s.nextInt();
+               result[i][j] = s.nextInt();
            }
        }
-       //System.out.println("Enter the second matrix");
+       return result;
+   }
+
+   public static int[][] add(int[][] a, int[][] b) {
+       int rows = a.length;
+       int columns = a[0].length;
+       int[][] result = new int[rows][columns];
        for (int i = 0; i < rows; i++) {
            for (int j = 0; j < columns; j++) {
-               b[i][j] = s.nextInt();
+               result[i][j] = a[i][j] + b[i][j];
            }
        }
-       int[][] c = new int[rows][columns];
-       for (int i = 0; i < rows; i++) {
-           for (int j = 0; j < columns; j++) {
-               c[i][j] = a[i][j] + b[i][j];
-           }
-       }
-       //System.out.println("The sum of the two matrices is");
-       for (int i = 0; i < rows; i++) {
-           for (int j = 0; j < columns; j++) {
-               System.out.print(c[i][j] + " ");
-           }
-           System.out.println();
-       }
+       return result;
    }
 }
 
