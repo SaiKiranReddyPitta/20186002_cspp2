8032cbb9f8666ee5f7c73d6f7d7525d487527695
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-30 13:49:14 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ assignment/m4/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Solution {
+	/* Fill the main function to print resultant of addition of matrices*/
+	public static void main(String[] args) {
+
+    int m, n, c, d;
+    Scanner in = new Scanner(System.in);
+    m = in.nextInt();
+    n  = in.nextInt();
+ 
+    int first[][] = new int[m][n];
+    int second[][] = new int[m][n];
+    int sum[][] = new int[m][n];
+ 
+      for (  c = 0 ; c < m ; c++ )
+         for ( d = 0 ; d < n ; d++ )
+            first[c][d] = in.nextInt();
+ 
+      for ( c = 0 ; c < m ; c++ )
+         for ( d = 0 ; d < n ; d++ )
+            second[c][d] = in.nextInt();
+ 
+      for ( c = 0 ; c < m ; c++ )
+         for ( d = 0 ; d < n ; d++ )
+             sum[c][d] = first[c][d] + second[c][d];  //replace '+' with '-' to subtract matrices
+ 
+//Addition of matrices.
+ 
+      for ( c = 0 ; c < m ; c++ )
+      {
+         for ( d = 0 ; d < n ; d++ )
+            System.out.print(sum[c][d]+"\t");
+ 
+         System.out.println();
+      }
+   }
+}
