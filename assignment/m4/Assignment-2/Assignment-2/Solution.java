bed5adc8a0c4aa4ed65c815bacf4ef9b7016fe95
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-30 22:33:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- assignment/m4/Assignment-2/Assignment-2/Solution.java
+++ assignment/m4/Assignment-2/Assignment-2/Solution.java
@@ -1,86 +1,69 @@
+/**Solution.java.
+ * @author Sai Kiran.
+*/
+/**
+*Adding two matrices.
+**/
 import java.util.Scanner;
-public class Solution
-{
-    public static void main(String[] args) 
-    {
-        int p, q, m, n;
+/**
+*used to take inputs.
+**/
+public final class Solution {
+    /**
+    *Empty constructor.
+    **/
+    private Solution() {
+    //I am not using this function
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+    public static void main(final String[] args) {
+        int rows1, cols1, rows2, cols2;
         Scanner s = new Scanner(System.in);
-        //System.out.print("Enter number of rows in first matrix:");
-        p = s.nextInt();
-        //System.out.print("Enter number of columns in first matrix:");
-        q = s.nextInt();
-
-        
-            int a[][] = new int[p][q];
-            
-
-            //System.out.println("Enter all the elements of first matrix:");
-            for (int i = 0; i < p; i++) 
-            {
-                for (int j = 0; j < q; j++) 
-                {
-                    a[i][j] = s.nextInt();
-                }
-            }
-        //System.out.print("Enter number of rows in second matrix:");
-        m = s.nextInt();
-        //System.out.print("Enter number of columns in second matrix:");
-        n = s.nextInt();
-           //System.out.println("Enter all the elements of second matrix:");
-            int b[][] = new int[m][n];
-            for (int i = 0; i < m; i++) 
-            {
-                for (int j = 0; j < n; j++) 
-                {
-                    b[i][j] = s.nextInt();
-                }
-            }
-           // System.out.println("First Matrix:");
-            // for (int i = 0; i < p; i++) 
-            // {
-            //     for (int j = 0; j < q; j++) 
-            //     {
-            //         //System.out.print("");/*(a[i][j]+" ");*/
-            //     }
-            //     //System.out.println("");
-            // }
-            //System.out.println("Second Matrix:");
-            for (int i = 0; i < m; i++) 
-            {
-                for (int j = 0; j < n; j++) 
-                {
-                    //System.out.print("");/*(b[i][j]+" ");*/
-                }
-                //System.out.println("");
-            }
-            if (p == m && q == n) 
-        	{
-            int c[][] = new int[m][n];            
-            for (int i = 0; i < p; i++) 
-            {
-                for (int j = 0; j < n; j++) 
-                {
-                    for (int k = 0; k < q; k++) 
-                    {
-                        c[i][j] = a[i][j] + b[i][j];
-                    }
-                }
-            }
-
-            //System.out.println("Matrix after addition:");
-
-            for (int i = 0; i < p; i++) 
-            {
-                for (int j = 0; j < n; j++) 
-                {
-                    System.out.print(c[i][j]+"");
-                }
-                System.out.println("");
+        rows1 = s.nextInt();
+        cols1 = s.nextInt();
+        int[][] a = new int[rows1][cols1];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols1; j++) {
+                a[i][j] = s.nextInt();
             }
         }
-        else
-        {
+        // for (int i = 0; i < rows1; i++) {
+        //     for (int j = 0; j < cols1; j++) {
+        //         System.out.print(a[i][j] + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        rows2 = s.nextInt();
+        cols2 = s.nextInt();
+        int[][] b = new int[rows2][cols2];
+        for (int i = 0; i < rows2; i++) {
+            for (int j = 0; j < cols2; j++) {
+                b[i][j] = s.nextInt();
+            }
+        }
+        // for (int i = 0; i < rows2; i++) {
+        //     for (int j = 0; j < cols2; j++) {
+        //         System.out.print(b[i][j] + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        int[][] c = new int[rows2][cols2];
+        if (rows1 == rows2 && cols1 == cols2) {
+            String res = "";
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < cols2; j++) {
+                        c[i][j] = a[i][j] + b[i][j];
+                        res += c[i][j] + " ";
+                    }
+                    res = res.trim() + "\n";
+            }
+            System.out.print(res);
+        } else {
             System.out.println("not possible");
         }
     }
 }
+
