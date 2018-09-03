eac4ea1d4172548ee0a2ff35c0674766fd1f3433
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-09-03 20:16:29 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- assignment/m7/Assignment-1/Assignment-1/Solution.java
+++ assignment/m7/Assignment-1/Assignment-1/Solution.java
@@ -1,27 +1,40 @@
+/**
+ *@author Sai Kiran Reddy Pitta.
+*/
 import java.util.*;
 class InputValidator
 {
-	/*Write the atrributes and methods for InputValidator*/
-	private String name;
-	InputValidator(String name) {
-		this.name = name;
-	}
-	boolean validateData() {
-		if(name.length()>6) {
-			return true;
-		}else{
-			return false;
-		}
-	}
+    /*
+     * Write the atrributes and methods for InputValidator
+     */
+    private String name;
+    InputValidator(String name) {
+        this.name = name;
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    boolean validateData() {
+        if(name.length()>6) {
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
+/**
+ * Class for solution.
+ */
 public class Solution
 {
-	public static void main(String args[])
+    public static void main(String args[])
     {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
-    	System.out.println(i.validateData());
+        Scanner s=new Scanner(System.in);
+        String input=s.next();
+        InputValidator i=new InputValidator(input);     
+        System.out.println(i.validateData());
 
     }
 
