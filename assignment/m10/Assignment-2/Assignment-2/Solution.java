6969443b5dd7fb90b663a581e663e714964b2c30
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-09-06 10:59:56 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- assignment/m10/Assignment-2/Assignment-2/Solution.java
+++ assignment/m10/Assignment-2/Assignment-2/Solution.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
     /**
      * Constructs the object.
      */
     private Solution() {
-        
+
     }
     /**.
      * { function_description }
