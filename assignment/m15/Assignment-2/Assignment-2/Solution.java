8b9726233f6740d36b1885f943534eddb0bc1c66
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-09-11 17:34:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignment/m15/Assignment-2/Assignment-2/Solution.java
+++ assignment/m15/Assignment-2/Assignment-2/Solution.java
@@ -6,9 +6,10 @@ import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
 class SortedSet extends Set  {
-    /**.
+    /**
      * sort function.
-     * @param      array  An array
+     *
+     * @param      array  The array
      */
     public void sort(final int[] array) {
         int t;
@@ -22,9 +23,10 @@ class SortedSet extends Set  {
             }
         }
     }
-    /**.
+    /**
      * add function.
-     * @param      item  An item
+     *
+     * @param      item  The item
      */
     public void add(final int item) {
         if (!contains(item)) {
@@ -32,18 +34,21 @@ class SortedSet extends Set  {
         }
         sort(set);
     }
-    /**.
-     * It's subset.
+    /**
+     * subset.
      *
-     * @param      start  An start
-     * @param      end    An end
+     * @param      start      The start
+     * @param      end        The end
+     *
      * @return     from start to end returns elements.
+     *
+     * @throws     Exception  {Invalid Arguments to Subset Exception }
      */
     public int[] subSet(final int start, final int end) throws Exception {
+
         if (start > end) {
-            throw new Exception ("Invalid Arguments to Subset Exception");
-           /* return null;*/
-        }
+            throw new Exception("Invalid Arguments to Subset Exception");
+        } else {
         int[] result = new int[size];
         int k = 0;
         for (int i = 0; i < size; i++) {
@@ -57,16 +62,22 @@ class SortedSet extends Set  {
             }
         }
         return Arrays.copyOf(result, k);
+        }
     }
-    /**.
-     *It's headset function.
-     * @param      end   An end
+    /**
+     * headset function.
+     *
+     * @param      end        The end
+     *
      * @return     returms elements.
+     *
+     * @throws     Exception  { set empty exception }
      */
     public int[] headSet(final int end) throws Exception {
+
         int[] result = new int[size];
         int temp = 0;
-        if (size == 0) {
+        if (size == 0 || end <= 1) {
             throw new Exception("Set Empty Exception");
         } else {
             for (int i = 0; i < size; i++) {
@@ -74,22 +85,28 @@ class SortedSet extends Set  {
                 result[i] = set[i];
                 temp++;
             }
-    }   }
+        }
+    }
         return Arrays.copyOf(result, temp);
     }
-    /**.
+    /**
      * last function.
+     *
      * @return     returns list of elements.
+     *
+     * @throws     Exception  set empty Exception
      */
     public int last() throws Exception {
+
         if (size == 0) {
             throw new Exception("Set Empty Exception");
             /*return -1;*/
         }
         return set[size - 1];
     }
-    /**.
+    /**
      * Adds all.
+     *
      * @param      element  The element
      */
     public void addAll(final int[] element) {
@@ -98,19 +115,21 @@ class SortedSet extends Set  {
         }
     }
 }
-/**.
+/**
  * Solution class.
  */
 public final class Solution {
-    /**.
+    /**
      * Constructs the object.
      */
     private Solution() {
 
     }
-    /**.
-     * helper function to convert string input to int array.
+    /**
+     * helper function which convert string input to int array.
+     *
      * @param      s     { string input from test case file }
+     *
      * @return     { int array from the given string }
      */
     public static int[] intArray(final String s) {
@@ -127,6 +146,7 @@ public final class Solution {
     }
     /**
      * main function to execute test cases.
+     *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
@@ -185,37 +205,33 @@ public final class Solution {
                 System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
                 break;
             case "subSet":
-            try {
 
                 if (tokens.length != 2) {
                     break;
                 }
                 String[] arrstring3 = tokens[1].split(",");
+            try {
                 int[] object = s.subSet(Integer.parseInt(arrstring3[0]),
                                         Integer.parseInt(arrstring3[1]));
-                if (object != null) {
                     System.out.println(Arrays.toString(object).replace("[",
                         "{").replace("]", "}"));
-                }
-                break;
-            } catch (Exception ex) {
+                } catch (Exception ex) {
                     System.out.println(ex.getMessage());
                 }
+                break;
             case "headSet":
-            try {
 
                 if (tokens.length != 2) {
                     break;
                 }
-                int[] obj = s.headSet(Integer.parseInt(tokens[1]));
-                if (obj != null) {
-                    System.out.println(Arrays.toString(obj).replace("[",
-                        "{").replace("]", "}"));
-                }
-                break;
-            } catch (Exception ex) {
+            try {
+                    int[] obj = s.headSet(Integer.parseInt(tokens[1]));
+                        System.out.println(Arrays.toString(obj).replace("[",
+                            "{").replace("]", "}"));
+                } catch (Exception ex) {
                     System.out.println(ex.getMessage());
                 }
+                break;
             case "last":
             try {
 
@@ -224,10 +240,10 @@ public final class Solution {
                 }
                 int temp = s.last();
                 System.out.println(temp);
-                break;
             } catch (Exception ex) {
                     System.out.println(ex.getMessage());
                 }
+                break;
             case "addAll":
                 int[] intArr = intArray(tokens[1]);
                 if (intArr.length == 1) {
