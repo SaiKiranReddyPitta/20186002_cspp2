4bf8ebba09f7ca9af42e5bd4be220b867bd4719d
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-09-17 18:22:27 +0530

    "check_style_score -> 5 of 13 passed. style score: 0/1 "

--- assignment/m20/Assignment-1/Assignment-1/Solution.java
+++ assignment/m20/Assignment-1/Assignment-1/Solution.java
@@ -259,7 +259,11 @@ public final class Solution {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
+        if(q != 0) {
         System.out.println(q + " " + "are added to the quiz");
+    } else {
+        System.out.println("Quiz does not have questions");        
+    }
         if (q > 0) {
             for (int i = 0; i < q; i++) {
                 String[] tokens = scan.nextLine().split(":");
